72f093101969535d1884c44674797a9c8e871b58
gedeastu <gede.astu@gmail.com>
2024-01-19 21:43:15 +0800

    setup and adding new var

--- kotlin-starter/src/main/kotlin/Main.kt
+++ kotlin-starter/src/main/kotlin/Main.kt
@@ -3,10 +3,11 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    var sapa:String = "Hello"
     val name:String = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println(sapa + ", " + name + "!")
 
 //    for (i in 1..5) {
 //        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
