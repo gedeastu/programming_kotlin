85b37627bde01d350f96fcf2bbacbea094e595e3
gedeastu <gede.astu@gmail.com>
2024-01-20 20:16:09 +0800

    functional part 1

--- kotlin-starter/src/main/kotlin/Functional Programming.kt
+++ kotlin-starter/src/main/kotlin/Functional Programming.kt
@@ -3,7 +3,14 @@ package org.example
 fun main() {
     var fullName = getFullName(first = "Gede", middle = "Astu", last = "Nugraha")
     println(fullName)
+    val number = sumNumber("Astu Nugraha",10,20,30,40,50)
+    print("${number}")
 }
 fun getFullName(first: String, middle: String, last: String):String{
     return "$first $middle $last"
+}
+
+//Vararg (Variable Argument)
+fun sumNumber(name: String,vararg number:Int):String{
+    return "$name ${number.sum()}"
 }
