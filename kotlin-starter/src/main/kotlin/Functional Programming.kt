c5cece5e8f8c5144a7e6e4a98072c9ea09d8cc20
gedeastu <gede.astu@gmail.com>
2024-01-21 00:17:36 +0800

    function type with typealias is done, on basic step

--- kotlin-starter/src/main/kotlin/Functional Programming.kt
+++ kotlin-starter/src/main/kotlin/Functional Programming.kt
@@ -1,11 +1,35 @@
 package org.example
 
+import kotlin.random.Random
+
 fun main() {
     var fullName = getFullName(first = "Gede", middle = "Astu", last = "Nugraha")
     println(fullName)
-    val number = sumNumber("Astu Nugraha",10,20,30,40,50)
-    print("${number}")
+
+    val numberListArray = intArrayOf(10, 20, 30, 40)
+    sets(numberListArray)
+
+    val number = sumNumber("Astu Nugraha",10,20,*numberListArray,40,50,)
+    println("${number}")
+
+    Random.nextInt(1, 15).printInt()
+
+    println(20.slice)
+
+    val age:Int? = null
+    println(age?.age)
+
+    val sum : Arithmetic = {valueA,valueB -> valueA + valueB }
+    val multiply : Arithmetic = {valueA,valueB -> valueA * valueB }
+
+    val sumResult = sum?.invoke(10, 10)
+    val multiplyResult = multiply?.invoke(20, 20)
+
+    println(sumResult)
+    println(multiplyResult)
 }
+
+//Basic Function
 fun getFullName(first: String, middle: String, last: String):String{
     return "$first $middle $last"
 }
@@ -14,3 +38,34 @@ fun getFullName(first: String, middle: String, last: String):String{
 fun sumNumber(name: String,vararg number:Int):String{
     return "$name ${number.sum()}"
 }
+
+//Array<T>
+fun sets(number: IntArray):IntArray{
+    return number
+}
+
+//Extensions
+//Extensions Function
+fun Int.printInt(){
+    if (this < 10){
+        println("the value is down of 10 = $this")
+    }else if (this == this){
+        println("the value is same of $this = $this")
+    }else{
+        println("the value is up of 10 = $this")
+    }
+}
+//Extensions Properties
+val Int.slice: Int
+    get() = this / 2
+
+//Nullable Receiver
+val Int.age: Unit
+    get() = if (this > 16){
+        println("U can watch movie with R rated")
+    }else{
+        println("U can't watch movie with R rated")
+    }
+
+//Function Type
+typealias Arithmetic = (Int, Int)->Int
